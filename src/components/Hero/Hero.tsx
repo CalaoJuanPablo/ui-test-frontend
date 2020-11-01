c053266f9e6979c4d2dccd7ec6d6126449711836
Juan Pablo Calao <jcalaop@gmail.com>
2020-11-01 16:29:07 -0500

    add hero card

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,12 +1,15 @@
 import { ReactElement } from 'react'
 import Header from '../Header/Header'
+import HeroCard from '../HeroCard/HeroCard'
 import styles from './Hero.module.scss'
 
 export default function Hero(): ReactElement {
   return (
     <section className={styles.Hero}>
       <Header isHome />
-      <div className={styles.Hero__container}>{/* Content */}</div>
+      <div className={styles.Hero__container}>
+        <HeroCard />
+      </div>
     </section>
   )
 }
---
+++ src/components/HeroCard/HeroCard.tsx
@@ -0,0 +1,33 @@
+import { ReactElement } from 'react'
+import styles from './HeroCard.module.scss'
+
+export default function HeroCard(): ReactElement {
+  return (
+    <div className={styles.HeroCard}>
+      <div className={styles.HeroCard__content}>
+        <small className={styles.HeroCard__pre_title}>
+          What&apos;s your opinion on
+        </small>
+        <h1 className={styles.HeroCard__title}>Pope Francis?</h1>
+        <p className={styles.HeroCard__description}>
+          He’s talking tough on clergy sexual abuse, but is he just another
+          papal pervert protector? (thumbs down) or a true pedophile punishing
+          pontiff? (thumbs up)
+        </p>
+        <a
+          className={styles.HeroCard__link}
+          href="https://wikipedia.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-wikipedia-w"></i>
+          <span className={styles['HeroCard__link--text']}>
+            More information
+          </span>
+        </a>
+        <h4 className={styles.HeroCard__question}>What&apos;s Your Veredit?</h4>
+      </div>
+      {/* <HeroVoter /> */}
+    </div>
+  )
+}
