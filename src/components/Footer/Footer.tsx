9efcb44fbe8d81836e1ca4e6a30c2bd7011eb721
Juan Pablo Calao <jcalaop@gmail.com>
2020-10-31 21:00:52 -0500

    add footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,54 @@
+import { ReactElement } from 'react'
+import Link from 'next/link'
+import styles from './Footer.module.scss'
+
+export default function Footer(): ReactElement {
+  return (
+    <footer className={styles.Footer}>
+      <div className={styles.Footer__container}>
+        <nav>
+          <ul className={styles['Footer__legal-menu']}>
+            <li className={styles['Footer__legal-menu--item']}>
+              <Link href="/terms">
+                <a>Terms and Conditions</a>
+              </Link>
+            </li>
+            <li className={styles['Footer__legal-menu--item']}>
+              <Link href="/privacy-policy">
+                <a>Privacy Policy</a>
+              </Link>
+            </li>
+            <li className={styles['Footer__legal-menu--item']}>
+              <Link href="/contact-us">
+                <a>Contact Us</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <nav className={styles.Footer__social}>
+          <span>Follow Us</span>
+          <ul className={styles['Footer__social-menu']}>
+            <li className={styles['Footer__social-menu--item']}>
+              <a
+                href="https://www.facebook.com/Zemoga"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-facebook-square"></i>
+              </a>
+            </li>
+            <li className={styles['Footer__social-menu--item']}>
+              <a
+                href="https://twitter.com/Zemoga"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-twitter"></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  )
+}
--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { ReactElement, ReactNode } from 'react'
 import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
 
 interface IMainLayout {
   children: ReactNode
@@ -12,9 +13,14 @@ export default function MainLayout({ children }: IMainLayout): ReactElement {
       <Head>
         <title>Rule of Thumb</title>
         <link rel="icon" href="/favicon.ico" />
+        <script
+          src="https://kit.fontawesome.com/4079afdc3f.js"
+          crossOrigin="anonymous"
+        />
       </Head>
       <Header />
       {children}
+      <Footer />
     </div>
   )
 }
