75ec0d21c9e679532c4fab93448df118dd4f1037
Juan Pablo Calao <jcalaop@gmail.com>
2020-11-01 09:18:58 -0500

    add votes bar styles

--- src/components/VotesBar/VotesBar.tsx
+++ src/components/VotesBar/VotesBar.tsx
@@ -1,12 +1,25 @@
 import { ReactElement } from 'react'
 import { IVotes } from './VotesBar.types'
+import styles from './VotesBar.module.scss'
 
 export default function VotesBar({ up, down }: IVotes): ReactElement {
   if (up + down === 100) {
     return (
-      <div>
-        <div>{up}%</div>
-        <div>{down}%</div>
+      <div className={styles.VotesBar}>
+        <div
+          className={styles['VotesBar__bar--up']}
+          style={{ width: `${up}%` }}
+        >
+          <i className="fas fa-thumbs-up"></i>
+          <span>{up}%</span>
+        </div>
+        <div
+          className={styles['VotesBar__bar--down']}
+          style={{ width: `${down}%` }}
+        >
+          <span>{down}%</span>
+          <i className="fas fa-thumbs-down"></i>
+        </div>
       </div>
     )
   }
